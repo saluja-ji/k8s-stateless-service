a10daedf2039646541c27b41f3d77dab04ec64a7
Pushpit Saluja <salujapushpit3@gmail.com>
2026-01-18 00:47:20 +0530

    Update deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+kubectl apply -f k8s/namespace.yaml
+kubectl apply -f k8s/configmap.yaml
+kubectl apply -f k8s/secret.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+kubectl apply -f k8s/hpa.yaml
+kubectl apply -f k8s/ingress.yaml
